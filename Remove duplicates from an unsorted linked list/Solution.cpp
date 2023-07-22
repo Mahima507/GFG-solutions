514f45bd39248c07349cd43cbcfc1f0e54596086
MAHIMA CHANDEL <75236972+Mahima507@users.noreply.github.com>
2023-07-22 21:21:51 +0530

    Create Solution.cpp

---
+++ Remove duplicates from an unsorted linked list/Solution.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) 
+    {
+     // your code goes here
+     map<int,bool>mp;
+     Node* tmp=head;
+     Node* t=tmp;
+     while(tmp!=NULL){
+        if(mp[tmp->data]==false)
+        {
+            mp[tmp->data]=true;
+            t=tmp; 
+            tmp=tmp->next;
+        }
+        else{
+            Node* del=t->next;
+            delete(del);
+            t->next=tmp->next;
+            tmp=tmp->next;
+        }
+     }
+     return head;
+    }
+};
