e8979b9beea93cd081c31784c85c9ba400cf66cd
MAHIMA CHANDEL <75236972+Mahima507@users.noreply.github.com>
2023-10-30 14:15:39 +0530

    Create Sum of XOR of all pairs.java

---
+++ Sum of XOR of all pairs.java
@@ -0,0 +1,23 @@
+//User function Template for Java
+       
+class Solution{
+    
+    // Function for finding maximum and value pair
+    public long sumXOR(int arr[], int n){
+     //Complete the function
+       long sum = 0;
+        for(int bit = 0; bit < 32; bit++){
+            long set_bits = 0, unset_bits = 0;
+            for(int j = 0; j < n; j++){
+                if((arr[j] & (1 << bit)) != 0){
+                    set_bits++;
+                }else{
+                    unset_bits++;
+                }
+            }
+            long possible_combinations = set_bits*unset_bits;
+            sum += possible_combinations*(1 << bit);
+        }
+        return sum;
+    }
+}
