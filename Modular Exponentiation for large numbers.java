8009b76e226b80b504f3b984ba1f4390735f7a95
MAHIMA CHANDEL <75236972+Mahima507@users.noreply.github.com>
2024-05-20 19:22:45 +0530

    Create Modular Exponentiation for large numbers.java

---
+++ Modular Exponentiation for large numbers.java
@@ -0,0 +1,16 @@
+//User function Template for Java
+
+class Solution
+{
+      public long PowMod(long x, long n, long m)
+    {
+        // Code here
+        long ans =1;
+	    while(n>0){
+	        if(n%2==1)ans=(ans*x)%m;
+	        x=(x*x)%m;
+	        n/=2;
+	    }
+	    return ans;
+    }
+}
