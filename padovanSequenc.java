aeba28a255d98016898bc30f99dd8a7b3858abcd
MAHIMA CHANDEL <75236972+Mahima507@users.noreply.github.com>
2024-06-13 21:38:07 +0530

    Create padovanSequenc.java

---
+++ padovanSequenc.java
@@ -0,0 +1,20 @@
+//User function Template for Java
+
+class Solution
+{
+  public int padovanSequence(int n)
+    {
+        //code here.
+       if(n<=2)return 1;
+       int a=1,b=1,c=1,temp=0,mod=1000000007;
+       for(int i=3;i<=n;i++){
+           temp=(a+b)%mod;
+           a=b;
+           b=c;
+           c=temp;
+       }
+       return c;
+    }
+
+    
+}
